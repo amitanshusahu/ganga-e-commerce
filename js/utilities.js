310c713d90667d2f596c4e19aed20a59a3248309
amitanshusahu <amitansusahu@gmail.com>
2021-09-10 10:49:55 +0530

    Updates- notification, and other pages added

--- js/utilities.js
+++ js/utilities.js
@@ -26,3 +26,22 @@
         document.getElementById("loginform").style="height:0%;"
      }
      //<----------------------------->>
+
+    //  for navigation box , NOT WORKING IN THE EXTERNAL BUT WORKING IN THE INTERNAL
+    //  var notificationClickNo = 1;
+    //  notification(notificationClickNo)
+
+    //  function openNotification(o){
+    //      notification(notificationClickNo += o)
+    //  }
+
+    //  function notification(o){
+    //     if(o%2==0){
+    //         document.getElementById("notification").style="display:block;"
+    //         console.log("hi it's working")
+    //     }
+    //     else{
+    //         document.getElementById("notification").style="display:none;"
+    //     }
+    //  }
+    //  <<---------------------------------------->
